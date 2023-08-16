c9a78f3388418d16cbce97eeba520853fd4dae36
Dekel Cohen <dcohen52@gmail.com>
2023-08-16 19:15:15 +0300

    Every line related to it's respective Scenario & Feature.

--- core.py
+++ core.py
@@ -1,6 +1,8 @@
 import pyparsing as pp
-from functions import FeatureLine, ScenarioLine, GivenLine, WhenLine, ThenLine, AndLine, OrLine
+from functions import ParsingContext, FeatureLine, ScenarioLine, GivenLine, WhenLine, ThenLine, AndLine, OrLine
 from helper_functions import ADD_KEYWORD
+
+context = ParsingContext()
 
 FEATURE = pp.Keyword('Feature').setResultsName('feature')
 SCENARIO = pp.Keyword('Scenario').setResultsName('scenario')
@@ -10,16 +12,13 @@ THEN = pp.Keyword('Then').setResultsName('then')
 AND = pp.Keyword('And').setResultsName('and')
 OR = pp.Keyword('Or').setResultsName('or')
 
-# ADD_KEYWORD('Example', 'example')
-
-
-feature_line = FEATURE + pp.restOfLine().setParseAction(FeatureLine().parse)
-scenario_line = SCENARIO + pp.restOfLine().setParseAction(ScenarioLine().parse)
-given_line = GIVEN + pp.restOfLine().setParseAction(GivenLine().parse)
-when_line = WHEN + pp.restOfLine().setParseAction(WhenLine().parse)
-then_line = THEN + pp.restOfLine().setParseAction(ThenLine().parse)
-and_line = AND + pp.restOfLine().setParseAction(AndLine().parse)
-or_line = OR + pp.restOfLine().setParseAction(OrLine().parse)
+feature_line = FEATURE + pp.restOfLine().setParseAction(FeatureLine(context).parse)
+scenario_line = SCENARIO + pp.restOfLine().setParseAction(ScenarioLine(context).parse)
+given_line = GIVEN + pp.restOfLine().setParseAction(GivenLine(context).parse)
+when_line = WHEN + pp.restOfLine().setParseAction(WhenLine(context).parse)
+then_line = THEN + pp.restOfLine().setParseAction(ThenLine(context).parse)
+and_line = AND + pp.restOfLine().setParseAction(AndLine(context).parse)
+or_line = OR + pp.restOfLine().setParseAction(OrLine(context).parse)
 
 feature_file_grammar = pp.StringStart() + pp.OneOrMore(
     pp.pythonStyleComment |
@@ -38,6 +37,11 @@ def parse_feature_file(file_path):
         with open(file_path, 'r') as file:
             feature_file = file.read()
             parsed_lines = feature_file_grammar.parseString(feature_file)
+
+            # Print the structure of the feature
+            if context.features:  # If we've parsed at least one feature
+                print(context.features[0])  # Print the first feature
+
             return parsed_lines.asList()
     except FileNotFoundError:
         print(f"File {file_path} not found!")
@@ -45,5 +49,3 @@ def parse_feature_file(file_path):
     except pp.ParseException as e:
         print(f"Error while parsing file {file_path} at line {e.lineno}, column {e.col}: {e.msg}")
         return []
-
-
--- functions.py
+++ functions.py
@@ -3,63 +3,96 @@
 #  Functionality to the lines  #
 ################################
 
+class Feature:
+    def __init__(self, title):
+        self.title = title
+        self.scenarios = []
+
+    def __str__(self):
+        return f"Feature: {self.title}\n" + "\n".join(str(scenario) for scenario in self.scenarios)
+
+    def add_scenario(self, scenario):
+        self.scenarios.append(scenario)
+
+
+class Scenario:
+    def __init__(self, title):
+        self.title = title
+        self.steps = []
+
+    def __str__(self):
+        steps_str = "\n".join(f"{step_type}: {content}" for step in self.steps for step_type, content in step.items())
+        return f"Scenario: {self.title}\n{steps_str}"
+
+    def add_step(self, step_type, content):
+        self.steps.append({step_type: content})
+
+
+class ParsingContext:
+    def __init__(self):
+        self.current_feature = None
+        self.current_scenario = None
+        self.features = []
+
+
 class FeatureLine:
+    def __init__(self, context):
+        self.context = context
+
     def parse(self, line):
-        print("Found Feature line:", line)
+        feature = Feature(line[0])
+        self.context.current_feature = feature
+        self.context.features.append(feature)
+        print("Found Feature line:", str(line).strip("['']"))
 
 
 class ScenarioLine:
-    def parse(self, line):
-        print("Found Scenario line:", line)
-        line_array = line[0].split(":")
-        if line_array[0].strip() == "Outline":
-            print("Found Scenario Outline line:", line)
-        else:
-            pass
-
-
-class GivenLine:
-    def __init__(self):
-        self.line_dispatcher = {
-            'the Maker has started a game with the word "silky"': self.maker_started_a_game
-        }
+    def __init__(self, context):
+        self.context = context
 
     def parse(self, line):
-        """
-        :param line: list[0] converted to a string.
-        :return: executes relevant method to the current line.
-        """
+        scenario = Scenario(line[0])
+        self.context.current_scenario = scenario
+        self.context.current_feature.add_scenario(scenario)
+        print("Found Scenario line:", str(line).strip("[':'] "))
+        if self.context.current_feature:
+            print("    Current Feature:", self.context.current_feature.title)
+
+
+class StepLine:
+    def __init__(self, context):
+        self.context = context
+        self.line_dispatcher = {}
+
+    def parse(self, line):
         curr_line = line[0]
-        line = curr_line[:2].replace(" ", "") + curr_line[2:]
-        # Retrieve method from dispatch dictionary and invoke it
-        method_to_invoke = self.line_dispatcher.get(line)
+        method_to_invoke = self.line_dispatcher.get(curr_line)
         if method_to_invoke:
             method_to_invoke()
+        # Also store the step in the current scenario
+        self.context.current_scenario.add_step(self.__class__.__name__, curr_line)
+        if self.context.current_scenario:
+            print("    Current Scenario:", self.context.current_scenario.title)
+
+
+class GivenLine(StepLine):
+    def __init__(self, context):
+        super().__init__(context)
+        self.line_dispatcher = {
+            'Given the Maker has started a game with the word "silky"': self.maker_started_a_game
+        }
 
     def maker_started_a_game(self):
         print('Method -> Given the Maker has started a game with the word "silky"')
 
 
-class WhenLine:
-
-    def __init__(self):
+class WhenLine(StepLine):
+    def __init__(self, context):
+        super().__init__(context)
         self.line_dispatcher = {
-            'the Maker starts a game': self.maker_waits_for_a_breaker,
-            "the Breaker joins the Maker's game": self.maker_joins_the_breaker
+            'When the Maker waits for a Breaker to join': self.maker_waits_for_a_breaker,
+            'When the Breaker joins the Maker\'s game': self.maker_joins_the_breaker
         }
-
-    def parse(self, line):
-        """
-
-        :param line: list[0] converted to a string.
-        :return: executes relevant method to the current line.
-        """
-        curr_line = line[0]
-        line = curr_line[:1].replace(" ", "") + curr_line[1:]
-        # Retrieve method from dispatch dictionary and invoke it
-        method_to_invoke = self.line_dispatcher.get(line)
-        if method_to_invoke:
-            method_to_invoke()
 
     def maker_waits_for_a_breaker(self):
         print("Method -> When the Maker starts a game")
@@ -68,26 +101,13 @@ class WhenLine:
         print("Method -> When the Breaker joins the Maker's game")
 
 
-class ThenLine:
-
-    def __init__(self):
+class ThenLine(StepLine):
+    def __init__(self, context):
+        super().__init__(context)
         self.line_dispatcher = {
-            'the Maker waits for a Breaker to join': self.maker_waits_for_a_breaker,
-            'the Breaker must guess a word with 5 characters': self.breaker_must_guess
+            'Then the Maker waits for a Breaker to join': self.maker_waits_for_a_breaker,
+            'Then the Breaker must guess a word with 5 characters': self.breaker_must_guess
         }
-
-    def parse(self, line):
-        """
-
-        :param line: list[0] converted to a string.
-        :return: executes relevant method to the current line.
-        """
-        curr_line = line[0]
-        line = curr_line[:1].replace(" ", "") + curr_line[1:]
-        # Retrieve method from dispatch dictionary and invoke it
-        method_to_invoke = self.line_dispatcher.get(line)
-        if method_to_invoke:
-            method_to_invoke()
 
     def maker_waits_for_a_breaker(self):
         print("Method -> Then the Maker waits for a Breaker to join")
@@ -97,10 +117,36 @@ class ThenLine:
 
 
 class AndLine:
+    def __init__(self, context):
+        self.context = context
+        self.line_dispatcher = {}
+
     def parse(self, line):
-        print("Found And line:", line)
+        """
+        :param line: list[0] converted to a string.
+        :return: executes relevant method to the current line.
+        """
+        curr_line = line[0]
+        line = curr_line[:1].replace(" ", "") + curr_line[1:]
+        # Retrieve method from dispatch dictionary and invoke it
+        method_to_invoke = self.line_dispatcher.get(line)
+        if method_to_invoke:
+            method_to_invoke()
 
 
 class OrLine:
+    def __init__(self, context):
+        self.context = context
+        self.line_dispatcher = {}
+
     def parse(self, line):
-        print("Found Or line:", line)
+        """
+        :param line: list[0] converted to a string.
+        :return: executes relevant method to the current line.
+        """
+        curr_line = line[0]
+        line = curr_line[:1].replace(" ", "") + curr_line[1:]
+        # Retrieve method from dispatch dictionary and invoke it
+        method_to_invoke = self.line_dispatcher.get(line)
+        if method_to_invoke:
+            method_to_invoke()
