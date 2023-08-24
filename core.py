4c9522273d2d4ebbe494c515e3389833a4d361f7
Dekel Cohen <dcohen52@gmail.com>
2023-08-24 08:56:45 +0300

    Base structure is more robust

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 import pyparsing as pp
-from functions import ParsingContext, FeatureLine, ScenarioLine, GivenLine, WhenLine, ThenLine, AndLine, OrLine
+from functions import ParsingContext, FeatureLine, ScenarioLine, GivenLine, WhenLine, ThenLine, AndLine, OrLine, \
+    ScenarioOutlineLine
 from helper_functions import ADD_KEYWORD
 
 context = ParsingContext()
@@ -11,6 +12,8 @@ WHEN = pp.Keyword('When').setResultsName('when')
 THEN = pp.Keyword('Then').setResultsName('then')
 AND = pp.Keyword('And').setResultsName('and')
 OR = pp.Keyword('Or').setResultsName('or')
+SCENARIO_OUTLINE = pp.Keyword('Scenario Outline').setResultsName('scenario_outline')
+EXAMPLES = pp.Keyword('Examples:').setResultsName('examples')
 
 feature_line = FEATURE + pp.restOfLine().setParseAction(FeatureLine(context).parse)
 scenario_line = SCENARIO + pp.restOfLine().setParseAction(ScenarioLine(context).parse)
@@ -19,11 +22,35 @@ when_line = WHEN + pp.restOfLine().setParseAction(WhenLine(context).parse)
 then_line = THEN + pp.restOfLine().setParseAction(ThenLine(context).parse)
 and_line = AND + pp.restOfLine().setParseAction(AndLine(context).parse)
 or_line = OR + pp.restOfLine().setParseAction(OrLine(context).parse)
+# example_row = pp.lineStart + pp.Group(pp.delimitedList(pp.Word(pp.alphas + '_"'))).setParseAction(
+#     ScenarioLine.add_example_row)
+# examples_grammar = EXAMPLES + pp.lineEnd + pp.OneOrMore(example_row)
+
+scenario_outline_line = SCENARIO_OUTLINE + pp.restOfLine().setParseAction(ScenarioOutlineLine(context).parse)
+
+pipe_delimited = pp.delimitedList(pp.originalTextFor(pp.Word(pp.alphas + '_" ')), delim="|", combine=True)
+example_header = pp.Suppress("|") + pipe_delimited + pp.Suppress("|") + pp.lineEnd()
+example_row = pp.Suppress("|") + pipe_delimited + pp.Suppress("|") + pp.lineEnd()
+examples_grammar = EXAMPLES + pp.lineEnd + example_header + pp.OneOrMore(example_row)
+
+# example_header = pp.Group(pp.delimitedList(pp.Word(pp.alphas + '_'))).setParseAction(
+#     ScenarioOutlineLine(context).add_example_header)
+# example_row = pp.Group(pp.delimitedList(pp.Word(pp.alphas + '_"'))).setParseAction(
+#     ScenarioOutlineLine(context).add_example_row)
+# examples_grammar = EXAMPLES + pp.lineEnd + example_header + pp.lineEnd + pp.OneOrMore(example_row)
+
+# pipe_delimited = pp.delimitedList(pp.Word(pp.printables + " "), delim="|", combine=True)
+# examples_header = EXAMPLES + pp.LineEnd() + pp.Suppress("|") + pipe_delimited + pp.Suppress("|") + pp.LineEnd()
+# examples_row = pp.Suppress("|") + pipe_delimited + pp.Suppress("|") + pp.LineEnd()
+# example_block = examples_header + pp.OneOrMore(examples_row)
+
 
 feature_file_grammar = pp.StringStart() + pp.OneOrMore(
     pp.pythonStyleComment |
     feature_line |
     scenario_line |
+    scenario_outline_line |
+    examples_grammar |
     given_line |
     when_line |
     then_line |
@@ -38,11 +65,44 @@ def parse_feature_file(file_path):
             feature_file = file.read()
             parsed_lines = feature_file_grammar.parseString(feature_file)
 
+            is_scenario_outline = False
+            scenario_outline_steps = []
+
+            for idx, line in enumerate(parsed_lines):
+                if 'scenario_outline' in line:
+                    is_scenario_outline = True
+                    print("\nScenario Outline:", line[1])  # Printing the scenario outline title
+                    continue
+
+                # If within a Scenario Outline block
+                if is_scenario_outline:
+                    if 'given' in line or 'when' in line or 'then' in line:
+                        scenario_outline_steps.append(line)
+                        continue
+
+                if 'examples' in line:
+                    headers = line[1]
+                    rows = [row for row in line[2:] if row != '\n']
+
+                    for row_idx, row in enumerate(rows):
+                        print(f"\nExample {row_idx + 1}/{len(rows)}:")
+                        for step in scenario_outline_steps:
+                            print(step[0], end=' ')  # Given, When, or Then
+                            step_text = step[1]
+                            for header, value in zip(headers, row):
+                                step_text = step_text.replace(f"<{header}>", value)
+                            print(step_text)
+
+                    # Resetting for the next Scenario or Scenario Outline
+                    is_scenario_outline = False
+                    scenario_outline_steps = []
+
             # Print the structure of the feature
-            if context.features:  # If we've parsed at least one feature
-                print(context.features[0])  # Print the first feature
+            if context.features:
+                print("\n", context.features[0])
 
             return parsed_lines.asList()
+
     except FileNotFoundError:
         print(f"File {file_path} not found!")
         return []
--- functions.py
+++ functions.py
@@ -2,6 +2,9 @@
 #    Edit this file to add     #
 #  Functionality to the lines  #
 ################################
+import re
+from colorama import Fore, Style, Back
+
 
 class Feature:
     def __init__(self, title):
@@ -19,10 +22,37 @@ class Scenario:
     def __init__(self, title):
         self.title = title
         self.steps = []
+        # FOR OUTLINE
+        self.examples_headers = []
+        self.examples_rows = []
 
     def __str__(self):
-        steps_str = "\n".join(f"{step_type}: {content}" for step in self.steps for step_type, content in step.items())
-        return f"Scenario: {self.title}\n{steps_str}"
+        steps_str = ""
+        for step in self.steps:
+            for step_type, content in step.items():
+                if self.examples_rows:
+                    for row in self.examples_rows:
+                        substituted_content = content
+                        for header, value in zip(self.examples_headers, row):
+                            substituted_content = substituted_content.replace(f"<{header}>", value)
+                        steps_str += f"{step_type}: {substituted_content}\n"
+                else:
+                    steps_str += f"{step_type}: {content}\n"
+
+        # Add the examples if they exist
+        examples_str = ""
+        if self.examples_headers:
+            examples_str += "Examples:\n| " + " | ".join(self.examples_headers) + " |\n"
+            for row in self.examples_rows:
+                examples_str += "| " + " | ".join(row) + " |\n"
+
+        return f"Scenario: {self.title}\n{steps_str}{examples_str}"
+
+    def add_examples_header(self, headers):
+        self.examples_headers = headers
+
+    def add_examples_row(self, row):
+        self.examples_rows.append(row)
 
     def add_step(self, step_type, content):
         self.steps.append({step_type: content})
@@ -40,10 +70,11 @@ class FeatureLine:
         self.context = context
 
     def parse(self, line):
-        feature = Feature(line[0])
+        title = str(line).split(":")[1].strip("' []")
+        feature = Feature(title)
         self.context.current_feature = feature
         self.context.features.append(feature)
-        print("Found Feature line:", str(line).strip("['']"))
+        print("Found Feature line:", title)
 
 
 class ScenarioLine:
@@ -51,57 +82,111 @@ class ScenarioLine:
         self.context = context
 
     def parse(self, line):
-        scenario = Scenario(line[0])
+        title = str(line).split(":")[1].strip("' []")
+        scenario = Scenario(title)
         self.context.current_scenario = scenario
         self.context.current_feature.add_scenario(scenario)
-        print("Found Scenario line:", str(line).strip("[':'] "))
-        if self.context.current_feature:
-            print("    Related to Feature:", self.context.current_feature.title)
+        print("Found Scenario line:", title)
+        print("    Related to Feature:", self.context.current_feature.title)
+
+    def add_example_row(self, row):
+        self.context.current_scenario.add_examples_row(row)
+
+
+class ScenarioOutlineLine(ScenarioLine):
+
+    def __init__(self, context):
+        self.context = context  # Assuming you want to store the parsing context
+        self.title = ""  # Assuming you store the title of the scenario outline
+        self.steps = []  # List to store steps like Given, When, Then
+        self.examples_header = []
+        self.examples_rows = []
+
+    def parse(self, line):
+        super().parse(line)  # Using the parse function from ScenarioLine
+
+    def add_examples_header(self, headers):
+        self.examples_header = headers
+
+    def add_examples_row(self, row):
+        self.examples_rows.append(row)
 
 
 class StepLine:
     def __init__(self, context):
         self.context = context
-        self.line_dispatcher = {}
+        self.pattern_dispatcher = []  # Moved the dispatcher to this level for better structure
 
     def parse(self, line):
         curr_line = line[0]
-        method_to_invoke = self.line_dispatcher.get(curr_line)
-        if method_to_invoke:
-            method_to_invoke()
-        # Also store the step in the current scenario
-        self.context.current_scenario.add_step(self.__class__.__name__, curr_line)
-        if self.context.current_scenario:
-            print("        Current Scenario:", self.context.current_scenario.title)
+        matched = False
+        for pattern, handler in self.pattern_dispatcher:
+            match = re.match(pattern, curr_line)
+            if match:
+                print(f"{Fore.LIGHTMAGENTA_EX}Matched pattern: {pattern} with line: {curr_line}{Style.RESET_ALL}")
+                handler(*match.groups())  # Applying the handler function when a match is found
+                # matched = True
+                break
+            if not matched:
+                print(f"No handler found for: {curr_line}")
 
-            if self.context.current_scenario.title == "Scenario Outline":
-                print("        Current Scenario Outline:", self.context.current_scenario.title)
-                print("        Current Scenario Outline Examples:", self.context.current_scenario.title)
+        # Store the step in the current scenario
+        self.context.current_scenario.add_step(self.__class__.__name__, curr_line)
+        print("        Current Scenario:", self.context.current_scenario.title)
+
+        # Additional handling if the current scenario is a Scenario Outline
+        if self.context.current_scenario.title == "Outline":  # Adjusted from "Scenario Outline" to "Outline" as we are parsing only the title
+            print("        Current Scenario Outline:", self.context.current_scenario.title)
+            print("        Current Scenario Outline Examples:", self.context.current_scenario.examples_rows)
 
 
 class GivenLine(StepLine):
     def __init__(self, context):
         super().__init__(context)
-        self.line_dispatcher = {
-            'Given the Maker has started a game with the word "silky"': self.maker_started_a_game
-        }
+        self.pattern_dispatcher = [
+            (r'\s*the Maker has started a game with the word "(.*)"', self.maker_started_a_game)
+        ]
 
-    def maker_started_a_game(self):
-        print('Method -> Given the Maker has started a game with the word "silky"')
+    def parse(self, line):
+        curr_line = line[0].strip()
+        # print(f"{Fore.LIGHTYELLOW_EX}DEBUG: Parsing '{curr_line}{Style.RESET_ALL}'")
+        matched = False
+        for pattern, handler in self.pattern_dispatcher:
+            match = re.match(pattern, curr_line.strip())
+            if match:
+                # print(f"{Fore.LIGHTBLUE_EX}DEBUG: Matched using {pattern}{Style.RESET_ALL}")
+                # print(f"{Fore.LIGHTMAGENTA_EX}Matched pattern: {pattern} with line: {curr_line}{Style.RESET_ALL}")
+                handler(*match.groups())
+                matched = True
+                break
+        if not matched:
+            # print(f"{Fore.LIGHTBLUE_EX}DEBUG: Failed to match {curr_line} using {pattern}{Style.RESET_ALL}")
+            print(f"{Fore.RED}No handler found for: {curr_line}{Style.RESET_ALL}")
+
+        # Store the step in the current scenario
+        self.context.current_scenario.add_step(self.__class__.__name__, curr_line)
+
+    def maker_started_a_game(self, word):
+        print(f"Method -> Given the Maker has started a game with the word {word}")
 
 
 class WhenLine(StepLine):
     def __init__(self, context):
         super().__init__(context)
         self.line_dispatcher = {
-            'When the Maker waits for a Breaker to join': self.maker_waits_for_a_breaker,
-            'When the Breaker joins the Maker\'s game': self.maker_joins_the_breaker
+            (r'When the Maker starts a game', self.maker_starts_a_game),
+            (r'When the Breaker guesses the word "(.*)"', self.breaker_guesses_word),
+            ("When the Breaker joins the Maker's game", self.breaker_joins_the_makers_game)
+
         }
 
-    def maker_waits_for_a_breaker(self):
+    def maker_starts_a_game(self):
         print("Method -> When the Maker starts a game")
 
-    def maker_joins_the_breaker(self):
+    def breaker_guesses_word(self, guess):
+        print(f"Method -> When the Breaker guesses the word {guess}")
+
+    def breaker_joins_the_makers_game(self):
         print("Method -> When the Breaker joins the Maker's game")
 
 
@@ -109,15 +194,19 @@ class ThenLine(StepLine):
     def __init__(self, context):
         super().__init__(context)
         self.line_dispatcher = {
-            'Then the Maker waits for a Breaker to join': self.maker_waits_for_a_breaker,
-            'Then the Breaker must guess a word with 5 characters': self.breaker_must_guess
+            (r'Then the outcome is "(.*)"', self.outcome_is),
+            ("Then the Maker waits for a Breaker to join", self.maker_waits_for_breaker_to_join),
+            (r'Then the Breaker must guess a word with "(.*)" characters', self.breaker_must_guess_a_word_with_n_chars),
         }
 
-    def maker_waits_for_a_breaker(self):
+    def outcome_is(self, outcome):
+        print(f"Method -> Then the outcome is {outcome}")
+
+    def maker_waits_for_breaker_to_join(self):
         print("Method -> Then the Maker waits for a Breaker to join")
 
-    def breaker_must_guess(self):
-        print("Method -> Then the Breaker must guess a word with 5 characters")
+    def breaker_must_guess_a_word_with_n_chars(self, n):
+        print(f"Method -> Then the Breaker must guess a word with {n} characters")
 
 
 class AndLine:
@@ -154,3 +243,24 @@ class OrLine:
         method_to_invoke = self.line_dispatcher.get(line)
         if method_to_invoke:
             method_to_invoke()
+
+
+class ExamplesLine:
+    def __init__(self, context):
+        self.context = context
+
+    def parse(self, lines, current_index):
+        # Assuming lines is the list of all lines and current_index is the index of the Examples: line
+        headers_line = lines[current_index + 1]
+        headers = [header.strip() for header in
+                   headers_line.split('|')[1:-1]]  # Splitting by '|' and ignoring the first and last empty elements
+
+        self.context.current_scenario.add_examples_header(headers)
+
+        # For the rows, continue till you hit a line that doesn't start and end with '|'
+        for line in lines[current_index + 2:]:
+            if line.startswith('|') and line.endswith('|'):
+                row = [value.strip() for value in line.split('|')[1:-1]]
+                self.context.current_scenario.add_examples_row(row)
+            else:
+                break
