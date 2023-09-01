85618d3f5c014df8759acc8d1a7056cf1a2aed1e
Dekel Cohen <dcohen52@gmail.com>
2023-09-01 20:06:42 +0300

    Selenium tests

--- run.py
+++ run.py
@@ -4,30 +4,44 @@
 #     file_path = 'example.feat'
 #     parsed_lines = parse_feature_file(file_path)
 
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import time
+# import pytest
+#
+# @pytest.fixture(scope="function")
+# def setup_driver():
+#     # Initialize Chrome WebDriver
+#     driver = webdriver.Chrome(executable_path="path/to/chromedriver")
+#     yield driver  # yield the setup (like return but for fixtures)
+#     driver.quit()  # teardown: this runs after the test function finishes
+#
+# def test_search_in_google(setup_driver):
+#     # Navigate to Google's homepage
+#     setup_driver.get("https://www.google.com")
+#
+#     # Find the search box element and enter "Selenium"
+#     search_box = setup_driver.find_element_by_name("q")
+#     search_box.send_keys("Selenium")
+#     search_box.send_keys(Keys.RETURN)
+#
+#     # Wait for search results to load
+#     time.sleep(2)
+#
+#     # Verify if "Selenium" exists in the first search result
+#     first_result = setup_driver.find_element_by_css_selector(".tF2Cxc").text
+#     assert "Selenium" in first_result, f"Expected 'Selenium' to be in first result, but got {first_result}"
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import pytest
 
-@pytest.fixture(scope="function")
-def setup_driver():
-    # Initialize Chrome WebDriver
-    driver = webdriver.Chrome(executable_path="path/to/chromedriver")
-    yield driver  # yield the setup (like return but for fixtures)
-    driver.quit()  # teardown: this runs after the test function finishes
-
-def test_search_in_google(setup_driver):
-    # Navigate to Google's homepage
-    setup_driver.get("https://www.google.com")
-
-    # Find the search box element and enter "Selenium"
-    search_box = setup_driver.find_element_by_name("q")
-    search_box.send_keys("Selenium")
-    search_box.send_keys(Keys.RETURN)
-
-    # Wait for search results to load
-    time.sleep(2)
-
-    # Verify if "Selenium" exists in the first search result
-    first_result = setup_driver.find_element_by_css_selector(".tF2Cxc").text
-    assert "Selenium" in first_result, f"Expected 'Selenium' to be in first result, but got {first_result}"
+driver = webdriver.Chrome(executable_path="C:\\chromedriver_win32\\chromedriver.exe")
+driver.get("https://www.google.com")
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+time.sleep(2)
+first_result = driver.find_element_by_css_selector(".tF2Cxc").text
+assert "Selenium" in first_result, f"Expected 'Selenium' to be in first result, but got {first_result}"
+driver.quit()
