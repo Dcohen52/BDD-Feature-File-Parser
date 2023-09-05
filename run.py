eaac24df415e9ae694eccdb6546fcc2362c20429
Dekel Cohen <dcohen52@gmail.com>
2023-09-05 18:27:27 +0300

    Config files

--- run.py
+++ run.py
@@ -3,3 +3,5 @@ from engine.core import parse_feature_file
 if __name__ == '__main__':
     file_path = 'lyre files/new_syntax.lyre'
     parsed_lines = parse_feature_file(file_path)
+
+# pip freeze > requirements.txt
